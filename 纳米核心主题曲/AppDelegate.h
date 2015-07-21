418b5a6ce860f8466cad18ed1256146faf60087c
cnspec <cnspec@cnspecdeMacBook-Pro.local>
2015-07-21 16:29:19 +0800

    Initial Commit

---
+++ 纳米核心主题曲/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  纳米核心主题曲
+//
+//  Created by cnspec on 15/7/21.
+//  Copyright (c) 2015年 Louise. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ 纳米核心主题曲/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  纳米核心主题曲
+//
+//  Created by cnspec on 15/7/21.
+//  Copyright (c) 2015年 Louise. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
